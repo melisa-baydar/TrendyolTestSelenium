30d2103f25f76cb58d8aeb40c46b1c86dc97e625
MELİSA BAYDAR <76971576+melisa-baydar@users.noreply.github.com>
2021-01-27 23:37:52 +0300

    Add files via upload

---
+++ TrendyolPage.java
@@ -0,0 +1,16 @@
+package Pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import StepDefinitions.LoginPageSteps;
+import io.cucumber.java.en.When;
+
+public class TrendyolPage {
+	
+	
+	
+}
