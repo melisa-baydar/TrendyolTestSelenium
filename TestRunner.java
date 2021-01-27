cb2a050ab1317115a57f1e2b011cab73254bff69
MELİSA BAYDAR <76971576+melisa-baydar@users.noreply.github.com>
2021-01-27 23:40:09 +0300

    Add files via upload

---
+++ TestRunner.java
@@ -0,0 +1,15 @@
+package StepDefinitions;
+
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/Features/Login.feature", glue = { "StepDefinitions" },
+strict = true,
+plugin={"pretty","junit:target/JUnitReports/report.xml"}
+)
+
+public class TestRunner {
+
+}
